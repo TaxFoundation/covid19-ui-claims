27e0e8598c367961299712f122da3010eb64524f
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-03-26 10:44:02 -0400

    Charts are drawn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 import { csvParse } from 'd3-dsv';
 
 import Selection from './components/Selection';
 import Chart from './components/Chart';
+
+const StyledApp = styled.div`
+  margin: 0 auto;
+  max-width: 1200px;
+`;
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -18,7 +24,7 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <StyledApp>
       {data.length && (
         <>
           <Selection
@@ -35,7 +41,7 @@ const App = () => {
           <Chart data={data.find(d => +d.id === selectedState)}></Chart>
         </>
       )}
-    </div>
+    </StyledApp>
   );
 };
 
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -8,12 +8,14 @@ import { Bar, Line } from '@vx/shape';
 const Chart = ({ data }) => {
   const height = 600;
   const width = 800;
-  const margin = { top: 20, right: 20, bottom: 20, left: 70 };
+  const margin = { top: 20, right: 20, bottom: 50, left: 70 };
   const xMax = width - margin.left - margin.right;
   const yMax = height - margin.top - margin.bottom;
+  const barData = [data['2008median'], data['2008max'], data['week20200326']];
+  const xLabels = ['2008 Median Week', '2008 Maximum Week', 'Week Ending 3/26/2020']
 
   const xScale = scaleBand({
-    domain: ['GR Median', 'GR Max', 'COVID-19'],
+    domain: xLabels,
     rangeRound: [0, xMax],
     padding: 0.4,
   });
@@ -22,9 +24,11 @@ const Chart = ({ data }) => {
     rangeRound: [yMax, 0],
     domain: [
       0,
-      Math.max(data['2008median'], data['2008max'], data['week20200326']),
+      Math.max(...barData),
     ],
   });
+
+  console.log(data);
 
   return (
     <svg viewBox={`0 0 ${width} ${height}`}>
@@ -32,12 +36,29 @@ const Chart = ({ data }) => {
         <AxisLeft
           top={margin.top}
           left={0}
+          labelOffset={margin.left - 20}
           scale={yScale}
           hideZero
           numTicks={10}
           label='Unemployment Insurance Claims'
         />
-        <AxisBottom scale={xScale} top={yMax} label={'Week of Claims'} />
+        <AxisBottom scale={xScale} top={margin.top + yMax} label={'Week of Claims'} />
+      </Group>
+      <Group>
+        {barData.map((bar, i) => {
+          const barWidth = xScale.bandwidth();
+          const barHeight = yMax - yScale(bar);
+          const barX = margin.left + xScale(xLabels[i]);
+          const barY = margin.top + yMax - barHeight;
+          return (<Bar
+            key={`bar-${bar}`}
+            x={barX}
+            y={barY}
+            width={barWidth}
+            height={barHeight}
+            fill="#0094ff"
+          />)
+        })}
       </Group>
     </svg>
   );
