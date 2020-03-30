9559c6053167e977a97ab993f79194711764b77d
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-03-30 09:33:05 -0400

    Set up for dynamic height embed with Pym

---
+++ public/pymSetup.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+  const pymParent = new pym.Parent(
+    'covid19-ui-claims-bar-chart',
+    'https://covid19-ui-claims-bar-chart.netlify.com//index.html',
+    {}
+  );
+});
