ee9a22c697c1199cf9f08641d4743b606c1df410
Tom VanAntwerp <tom@tomvanantwerp.com>
2020-04-16 11:07:45 -0400

    Move tooltip position out of styled component

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -5,12 +5,10 @@ const StyledTooltip = styled.div`
   background-color: white;
   border: 1px solid #efefef;
   border-radius: 4px;
-  top: ${props => (props.show ? `${props.y - 170}px` : 0)};
   color: #333;
   display: ${props => (props.show ? 'block' : 'none')};
   font-family: 'Lato', sans-serif;
   font-size: 0.7rem;
-  left: ${props => (props.show ? `${props.x}px` : 0)};
   padding: 0.5rem;
   position: fixed;
   width: 100px;
@@ -18,7 +16,13 @@ const StyledTooltip = styled.div`
 
 const Tooltip = ({ show, children, x, y }) => {
   return (
-    <StyledTooltip show={show} x={x} y={y}>
+    <StyledTooltip
+      show={show}
+      style={{
+        top: show ? `${y - 170}px` : 0,
+        left: show ? `${x - 50}px` : 0,
+      }}
+    >
       {children}
     </StyledTooltip>
   );
